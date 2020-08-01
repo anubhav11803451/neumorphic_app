1f01f3b001cf7d4c61fbe7c17a9242eeccd615f5
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-08-01 12:17:20 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicApp();
+  }
+}
